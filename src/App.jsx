86369777cad6deffc9db11896751e777827da06a
Spencer Holtaway <spencerholtaway@Spencers-MacBook.local>
2026-03-05 22:27:44 -0800

    Fix: switch to HashRouter for GitHub Pages, remove footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import LiveMap from './pages/LiveMap'
 import StopInfo from './pages/StopInfo'
@@ -9,7 +9,6 @@ function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-gray-50">
-        {/* Routes */}
         <main className="flex-1">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -19,13 +18,6 @@ function App() {
             <Route path="/routes" element={<RouteExplorer />} />
           </Routes>
         </main>
-
-        {/* Footer */}
-        <footer className="bg-gray-800 text-white py-4">
-          <div className="max-w-7xl mx-auto px-4 text-center">
-            <p>Marin Transit Data Visualizations | Powered by 511.org</p>
-          </div>
-        </footer>
       </div>
     </Router>
   )
